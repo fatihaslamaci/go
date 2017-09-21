37152a9aca33cb9b46cf4efd571387c234805332
fatihaslamaci <fatihaslamaci@gmail.com>
2017-09-21 23:05:52 +0300

    git ignore  add gogland ide settings

---
+++ caritakip/login.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"github.com/gorilla/securecookie"
+	"net/http"
+)
+
+var cookieHandler = securecookie.New(
+	securecookie.GenerateRandomKey(64),
+	securecookie.GenerateRandomKey(32))
+
+func loginPageHandler(response http.ResponseWriter, request *http.Request) {
+	context := Context{Title: "Login Page"}
+	renderBlank(response, "login", "blank_js", context)
+}
+
+func getUserName(request *http.Request) (userName string) {
+	if cookie, err := request.Cookie("session"); err == nil {
+		cookieValue := make(map[string]string)
+		if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
+			userName = cookieValue["email"]
+		}
+	}
+	return userName
+}
+
+func setSession(userName string, response http.ResponseWriter) {
+	value := map[string]string{
+		"email": userName,
+	}
+	if encoded, err := cookieHandler.Encode("session", value); err == nil {
+		cookie := &http.Cookie{
+			Name:  "session",
+			Value: encoded,
+			Path:  "/",
+		}
+		http.SetCookie(response, cookie)
+	}
+}
+
+func clearSession(response http.ResponseWriter) {
+	cookie := &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		Path:   "/",
+		MaxAge: -1,
+	}
+	http.SetCookie(response, cookie)
+}
+
+// login handler
+
+func loginHandler(response http.ResponseWriter, request *http.Request) {
+	name := request.FormValue("email")
+	pass := request.FormValue("password")
+	redirectTarget := "/"
+	if name != "" && pass != "" {
+		// .. check credentials ..
+		setSession(name, response)
+		redirectTarget = "/main"
+	}
+	http.Redirect(response, request, redirectTarget, 302)
+}
--- caritakip/main.go
+++ caritakip/main.go
@@ -1,7 +1,147 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var templatesNavbar = template.Must(template.ParseFiles(
+	"./templates/basenavbar.html",
+	"./templates/head.html",
+	"./templates/basefooter.html"))
+
+var templatesBlank = template.Must(template.ParseFiles(
+	"./templates/baseblank.html",
+	"./templates/head.html",
+	"./templates/basefooter.html"))
+
+type Context struct {
+	Title string
+}
+
+type CariKart struct {
+	Message string
+	Unvan   string
+	Kod     string
+}
+
+var CariKart1 CariKart
+
+func render(w http.ResponseWriter, tmpl string, js_tmpl string, context interface{}, template *template.Template) {
+	tmpl_list := []string{fmt.Sprintf("views/%s.html", tmpl),
+		fmt.Sprintf("templates/%s.html", js_tmpl),
+	}
+
+	t, err := template.Clone()
+	if err != nil {
+		log.Print("template clone error: ", err)
+	}
+
+	t, err = t.ParseFiles(tmpl_list...)
+	if err != nil {
+		log.Print("template parsing error: ", err)
+	}
+	err = t.Execute(w, context)
+	if err != nil {
+		log.Print("template executing error: ", err)
+
+	}
+}
+
+func renderBlank(w http.ResponseWriter, tmpl string, js_tmpl string, context interface{}) {
+	render(w, tmpl, js_tmpl, context, templatesBlank)
+}
+
+func renderNavbar(w http.ResponseWriter, tmpl string, js_tmpl string, context interface{}) {
+	render(w, tmpl, js_tmpl, context, templatesNavbar)
+}
+
+func blankHandler(w http.ResponseWriter, req *http.Request) {
+	context := Context{Title: "Blak Page"}
+
+	renderNavbar(w, "blank", "blank_js", context)
+}
+
+func carikartHandler(response http.ResponseWriter, request *http.Request) {
+
+	renderNavbar(response, "carikart", "blank_js", CariKart1)
+}
+
+func carikartHandlerPost(response http.ResponseWriter, request *http.Request) {
+
+	CariKart2 := CariKart1
+	CariKart2.Unvan = request.FormValue("unvan")
+	CariKart2.Kod = request.FormValue("kod")
+
+	if len(CariKart2.Kod) > 0 {
+		CariKart1 = CariKart2
+	} else {
+		CariKart2.Message = "Lütfen Zorunlu alanları giriniz"
+	}
+	renderNavbar(response, "carikart", "blank_js", CariKart2)
+
+}
+
+func indexPageHandler(response http.ResponseWriter, request *http.Request) {
+	context := Context{Title: "Login Page"}
+	renderBlank(response, "login", "blank_js", context)
+}
+
+func internalPageHandler(response http.ResponseWriter, request *http.Request) {
+	userName := getUserName(request)
+	context := Context{Title: "Home Page"}
+	if userName != "" {
+		renderNavbar(response, "main", "blank_js", context)
+	} else {
+		http.Redirect(response, request, "/", 302)
+	}
+
+}
+
+// logout handler
+
+func logoutHandler(response http.ResponseWriter, request *http.Request) {
+	clearSession(response)
+	http.Redirect(response, request, "/", 302)
+}
+
+// server main method
+
+var router = mux.NewRouter()
+
+func addStaticDir(s string) {
+	http.Handle("/"+s+"/", http.StripPrefix("/"+s, http.FileServer(http.Dir("./statics/"+s))))
+}
+
+func addStaticDirAll() {
+	addStaticDir("bower_components")
+	addStaticDir("dist")
+	addStaticDir("js")
+	addStaticDir("less")
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+
+	CariKart1.Kod = "ilk kod"
+	CariKart1.Unvan = "ilk ünvan"
+
+	router.HandleFunc("/", indexPageHandler)
+	router.HandleFunc("/loginpage", loginPageHandler)
+	router.HandleFunc("/login", loginHandler).Methods("POST")
+	router.HandleFunc("/logout", logoutHandler)
+	router.HandleFunc("/main", internalPageHandler)
+
+	router.HandleFunc("/blank", blankHandler)
+
+	router.HandleFunc("/carikart", carikartHandler)
+	router.HandleFunc("/carikartkaydet", carikartHandlerPost).Methods("POST")
+
+	http.Handle("/", router)
+
+	addStaticDirAll()
+
+	http.ListenAndServe(":8000", nil)
 }
